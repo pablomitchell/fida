dd6c451900f3a05f400152f9f068e8375461c1dc
pablomitchell <pablo.mitchell@gmail.com>
2021-02-04 10:22:33 -0800

    Refactored setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='fida',
     version='0.1',
     packages=['fida'],
-    url='',
-    license='License?',
+    url='https://github.com/pablomitchell/fida',
+    license='License...?',
     author='Pablo Mitchell',
     author_email='pablo.mitchell@gmail.com',
     description='Financial Data Accessor Code in Python'
