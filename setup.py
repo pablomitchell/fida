98c9be65165038c762612f22a1a8787f7447f42b
pablo mitchell <pablo.mitchell@integratedquant.com>
2021-12-03 10:52:50 -0800

    Updated setup.py with install_requires field

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='fida',
     version='0.1',
-    packages=['fida'],
+    packages=find_packages(),
     url='https://github.com/pablomitchell/fida',
-    license='License...?',
+    license='MIT',
     author='Pablo Mitchell',
     author_email='pablo.mitchell@gmail.com',
-    description='Financial Data Accessor Code in Python'
+    description='Financial Data Accessor Code in Python',
+    install_requires=[
+        'lxml',
+        'matplotlib',
+        'numba',
+        'pandas',
+        'pandas-datareader',
+        'pyarrow',
+        'scipy',
+        'setuptools',
+        'tqdm',
+        'tiingo',
+    ],
 )
