a4a0dca53954d12cbb095063216fedbf8724c624
pablomitchell <pablo.mitchell@gmail.com>
2023-09-12 23:26:04 -0700

    fixed pandas version issue

--- example.py
+++ example.py
@@ -12,15 +12,6 @@ end = "2020-12-31"
 sp500 = constituents.get_sp500()
 print(sp500.sample(10))
 
-# sp600 = constituents.get_sp500()
-# print(sp600.sample(10))
-
-sp500 = ohlcv.get_price_yahoo(symbols=sp500.ticker[:10], start=start, end=end)
-sp500 = sp500.dropna(axis="index", how="all").dropna(axis="columns", how="all")
+sp500_prices = ohlcv.get_price_tiingo(symbols=sp500.ticker[:10], start=start, end=end)
+sp500_prices = sp500_prices.dropna(axis="index", how="all").dropna(axis="columns", how="all")
 sp500.info()
-
-
-# import yfinance as yf
-
-# df = yf.download("AAPL", start=start, end=end)
-# print(df.tail().to_string())
