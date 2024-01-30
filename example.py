f9c765dbd35de21aabd6a5b5a4c4c894142aa5b0
pablomitchell <pablo.mitchell@gmail.com>
2024-01-30 09:58:06 -0800

    updated

--- example.py
+++ example.py
@@ -5,13 +5,20 @@ from fida import constituents, ohlcv
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
-start = "1990-01-01"
-end = "2020-12-31"
+params = {
+    "symbols": constituents.get_sp500().sample(10).ticker,
+    "start": "1990-01-01",
+    "end": "2020-12-31",
+    "interpolate": True,
+}
 
+sp_subset_prices = ohlcv.get_price_tiingo(**params)
+sp_subset_returns = ohlcv.get_return_tiingo(**params)
 
-sp500 = constituents.get_sp500()
-print(sp500.sample(10))
-
-sp500_prices = ohlcv.get_price_tiingo(symbols=sp500.ticker[:10], start=start, end=end)
-sp500_prices = sp500_prices.dropna(axis="index", how="all").dropna(axis="columns", how="all")
-sp500.info()
+print("PRICES")
+print(sp_subset_prices.head().to_string())
+print(sp_subset_prices.tail().to_string())
+print("-" * 80)
+print("RETURNS")
+print(sp_subset_returns.head().to_string())
+print(sp_subset_returns.tail().to_string())
